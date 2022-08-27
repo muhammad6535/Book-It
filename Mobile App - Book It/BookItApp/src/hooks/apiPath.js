15c7572515b9f2f6dd42d648afc3c2c783a00739
Muhammad Mhameed <mohmed6535@gmail.com>
2022-08-27 22:42:11 +0300

    apk

--- Mobile App - Book It/BookItApp/src/hooks/apiPath.js
+++ Mobile App - Book It/BookItApp/src/hooks/apiPath.js
@@ -1,2 +1,3 @@
-const apiPath = "http://10.0.2.2:5001/api";
+// const apiPath = "http://10.0.2.2:5001/api";
+const apiPath = "https://muhammad6535-001-site1.btempurl.com/api";
 export default apiPath;
